ebb22ccc3ae973fb8042113bbe42eb3d916518df
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-06-01 18:33:26 +0900

    Update 이웃한칸.java

--- bongbong/week_1/이웃한칸.java
+++ bongbong/week_1/이웃한칸.java
@@ -11,7 +11,7 @@ class Solution {
         for (int i = 0; i <= 3; i++) {
             int h_check = h + dh[i];
             int w_check = w + dw[i];
-            if (h_check >= 0 & h_check < n & w_check>=0 & w_check<n) {
+            if (h_check >= 0 && h_check < n && w_check>=0 & w_check<n) {
                 if (board[h][w].equals(board[h_check][w_check])) {
                     count++;
                 }

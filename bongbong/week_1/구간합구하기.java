83c3a97aebd7c49dc285fb881b59b91227492013
kevinlee <105219462+kiv9908@users.noreply.github.com>
2024-06-02 16:14:20 +0900

    Update 구간합구하기.java

--- bongbong/week_1/구간합구하기.java
+++ bongbong/week_1/구간합구하기.java
@@ -1,24 +1,38 @@
 package week_1.구간합구하기;
-//P11720
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
 
-    public static void main(String[] arg) {
-        Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        String numbers = sc.next();
-        sc.close();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in)); //선언
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken()); //수의 개수 N
+        int M = Integer.parseInt(st.nextToken()); //합을 구해야 하는 횟수 M
+        int[] arr = new int[N + 1];
+        List<Integer> result = new ArrayList<>();
+        arr[0] = 0;
 
-        int sum = 0;
-        for (int i = 0; i < N; i++) {
-            sum += Integer.parseInt(numbers.charAt(i)+"");
+        st = new StringTokenizer(bf.readLine());
+        // arr에 1~현재 인덱스까지의 구간합 저장
+        for (int i = 1; i <= N; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(sum);
-
-
-
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(bf.readLine());
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+//            System.out.println(arr[e]);
+//            System.out.println(arr[s-1]);
+            result.add(arr[e] - arr[s - 1]);
+        }
+        for (int i : result) {
+            System.out.println(i);
+        }
     }
 }
+

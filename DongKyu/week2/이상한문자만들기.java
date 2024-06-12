e88652cbd7e70f812b98474c84e1a60f702c7899
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-06-12 19:06:10 +0900

    Update 이상한문자만들기.java

--- DongKyu/week2/이상한문자만들기.java
+++ DongKyu/week2/이상한문자만들기.java
@@ -3,23 +3,21 @@ class Solution {
         String[] arr = s.split("");
         String answer = "";
         int idx = 0;
-        
-        for(int i=0; i<s.length();i++) {
-			if(" ".equals(arr[i])) {
-				answer += " ";
-				idx = 0;
-			}
-			else {
-				if(idx%2!=0) {
-					answer += arr[i].toLowerCase();
-				}
-				else {
-					answer += arr[i].toUpperCase();
-				}
-				idx++;
-			}
-		}
-        
+
+        for (int i = 0; i < s.length(); i++) {
+            if (" ".equals(arr[i])) {
+                answer += " ";
+                idx = 0;
+            } else {
+                if (idx % 2 != 0) {
+                    answer += arr[i].toLowerCase();
+                } else {
+                    answer += arr[i].toUpperCase();
+                }
+                idx++;
+            }
+        }
+
         return answer;
     }
 }

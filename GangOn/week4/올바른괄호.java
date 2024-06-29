8f7131c0be3f5481943241c5ef76819a7868bd4c
kimgo816 <128129450+kimgo816@users.noreply.github.com>
2024-06-29 16:30:40 +0900

    Create 올바른괄호.java

---
+++ GangOn/week4/올바른괄호.java
@@ -0,0 +1,23 @@
+class Solution {
+    boolean solution(String s) {
+        
+  		int count = 0;
+  		
+  		for(int i = 0; i < s.length(); i++) {
+  			if(String.valueOf(s.charAt(i)).equals("(")) {
+  				count++;
+  			} else {
+  				count--;
+  			}
+  			if(count < 0) {
+  				return false;
+  			}
+  		}
+  		
+  		if(count == 0) {
+  			return true;
+  		} else {
+  			return false;
+  		}
+    }
+}

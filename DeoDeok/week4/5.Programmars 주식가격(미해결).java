810bf1c09a15f881d94406b7f8abeeac04bc38f6
shseo232 <129025123+shseo232@users.noreply.github.com>
2024-07-01 16:41:51 +0900

    Update 5.Programmars 주식가격(미해결).java

--- DeoDeok/week4/5.Programmars 주식가격(미해결).java
+++ DeoDeok/week4/5.Programmars 주식가격(미해결).java
@@ -1,20 +1,22 @@
 import java.util.*;
-
 class Solution {
-    public int[] solution(int[] prices) {
+    public static int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        for(int i=0; i<prices.length;i++){
-            Stack<Integer>stack = new Stack();
+        Stack<Integer> stack = new Stack<>();
+
+
+        for (int i = 0; i < prices.length; i++) {
+            int count = 0;
             stack.push(prices[i]);
-            for(int j=i+1; j<prices.length;j++){
-                if(!stack.isEmpty() && stack.peek()<prices[j]){
-                    stack.push(prices[j]);
+            for (int j = i + 1; j < prices.length; j++) {
+                count++;//넘어가는 순간 1초
+                if(stack.peek() > prices[j]) {
+                    break;
                 }
             }
-            answer[i] = stack.size();
+            answer[i] = count;
         }
-        
-        
+
         return answer;
     }
 }

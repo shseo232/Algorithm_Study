cb4f962fd05255de9df908e930846f7f5aed6301
shseo232 <129025123+shseo232@users.noreply.github.com>
2024-07-22 19:45:23 +0900

    Update and rename 1.test to 1.적어도 대부분의 배수.java

---
+++ DeoDeok/week7/완전탐색/1.적어도 대부분의 배수.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import static java.util.Collections.list;
+
+/*
+문제
+다섯 개의 자연수가 있다. 이 수의 적어도 대부분의 배수는 위의 수 중 적어도 세 개로 나누어 지는 가장 작은 자연수이다.
+
+서로 다른 다섯 개의 자연수가 주어질 때, 적어도 대부분의 배수를 출력하는 프로그램을 작성하시오.
+
+입력
+첫째 줄에 다섯 개의 자연수가 주어진다. 100보다 작거나 같은 자연수이고, 서로 다른 수이다.
+
+출력
+첫째 줄에 적어도 대부분의 배수를 출력한다.
+
+예제 입력 1
+30 42 70 35 90
+예제 출력 1
+210
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        List<Integer> list = new ArrayList<Integer>();
+        while (st.hasMoreTokens()) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+            int count =0;
+            boolean flag = true;
+            int num =0;
+            while(flag){
+                count =0;
+                num++;
+                for(int j=0; j < list.size(); j++){
+                    if(num%list.get(j)==0){
+                        count++;
+                        if(count==3){
+                            System.out.println(num);
+                            flag = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
